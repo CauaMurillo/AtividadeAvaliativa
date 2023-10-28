b290cfebab7de1a102fd4914325128a6d8031e08
Cauã Murillo <149210068+CauaMurillo@users.noreply.github.com>
2023-10-27 22:14:26 -0300

    Interface e parte do codigo
    Aqui estou mostrando o codigo terminado certinho, a interface terminada e tudo rodando certinho!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(24, 29);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.TabIndex = 0;
+            this.lblPeso.Text = "Peso";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(72, 29);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 1;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(27, 75);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 2;
+            this.lblAltura.Text = "Altura";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(72, 75);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(27, 138);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(184, 61);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(42, 230);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(147, 20);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Location = new System.Drawing.Point(10, 237);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(26, 13);
+            this.lblIMC.TabIndex = 6;
+            this.lblIMC.Text = "IMC";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(27, 119);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(250, 278);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblIMC);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblPeso);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,40 @@ namespace AtividadeAvaliativa
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double peso, altura, resultado;
+            peso = double.Parse(txtPeso.Text);
+            altura = double.Parse(txtAltura.Text);
+
+            resultado = peso / (altura * altura);
+
+            txtResultado.Text = resultado.ToString();
+
+            if (resultado <= 18.5)
+            {
+                lblResultado.Text = ("Abaixo do peso");
+            }else if(resultado <  24.9)
+            {
+                lblResultado.Text = ("Peso ideal(parabéns)");
+            }else if(resultado < 29.9)
+            {
+                lblResultado.Text = ("Levemente acima do peso");
+            }else if(resultado < 34.9)
+            {
+                lblResultado.Text = ("Obesidade grau 1");
+            }else if(resultado < 39.9)
+            {
+                lblResultado.Text = ("Obesidade grau 2( severa)");
+            }else 
+            {
+                lblResultado.Text = ("Obesidade 3(mórbida)");
+            }
+          
+            
+
+            
+        }
     }
 }
